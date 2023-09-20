3103d71d2aab86bf1684c8ebff27319a343d9b84
蒲立望60263 <liwang.pu@going-link.com>
2023-09-20 17:55:53 +0800

    添加webpack文件输出

--- apps/toolkit-test/webpack.config.js
+++ apps/toolkit-test/webpack.config.js
@@ -1,9 +1,20 @@
 const { composePlugins, withNx } = require('@nx/webpack');
 const { withReact } = require('@nx/react');
+const path = require('path');
+const fs = require('fs');
+
+const saveWebpackJson = (config) => {
+  const tmpDir = path.resolve(__dirname, 'tmp');
+  if (!fs.existsSync(tmpDir)) {
+    fs.mkdirSync(tmpDir);
+  }
+  fs.writeFileSync(path.resolve(tmpDir, `webpack.json`), JSON.stringify(config), { encoding: 'utf-8', });
+};
 
 // Nx plugins for webpack.
 module.exports = composePlugins(withNx(), withReact(), (config) => {
   // Update the webpack config as needed here.
   // e.g. `config.plugins.push(new MyPlugin())`
+  saveWebpackJson(config);
   return config;
 });
